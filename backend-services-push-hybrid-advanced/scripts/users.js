0e0b95aec97ce16e50340b5fa30f9147d90147c0
Anton Dobrev <anton.dobrev@telerik.com>
2014-05-30 09:19:50 -0500

    Updated the PushPlugin with the latest version from https://github.com/phonegap-build/PushPlugin.

--- backend-services-push-hybrid-advanced/scripts/users.js
+++ backend-services-push-hybrid-advanced/scripts/users.js
@@ -1,41 +1,17 @@
-//(function (global) {
-//    'use strict';
-//    var app = global.app = global.app || {};
-//    app.usersModel = (function () {
-//        var usersData,
-//        	currentUser = kendo.observable({ data: null });
-//        var loadUsers = function () {
-//            return app.everlive.Users.currentUser().then(function (data) {
-//                //var currentUserData = data.result;
-//                //currentUserData.PictureUrl = app.resolveProfilePictureUrl(currentUserData.Picture);
-//                //currentUser.set('data', currentUserData);
-//                return app.everlive.Users.get();
-//            }).then(function (data) {
-//                usersData = new kendo.data.ObservableArray(data.result);
-//            }).then(null, function (err) {
-//                	app.showError(err.message);
-//            	}
-//            );
-//        };
-//        return {
-//            load: loadUsers,
-//            users: function () {
-//                return usersData;
-//            },
-//            currentUser: currentUser
-//        };
-//    }());
-//}(window));
-app.usersModel = (function (global) {
+(function (global) {
+    'use strict';
+    var app = global.app = global.app || {};
     
-    var usersViewModel = kendo.observable({
-        usersDataSource: app.usersDataSource,
-        onUserSelected: function (e) {
+    app.usersModel = (function () {
+        
+        var onUserSelected = function (e) {
             var isSelected = e.dataItem.get("isSelected");
             var newState = isSelected ? false : true;
             e.dataItem.set("isSelected", newState);
-        },
-        getSelectedUsersFromDataSource: function () {
+        };
+        
+        // TODO - refactor this function
+        var getSelectedUsersFromDataSource = function () {
             var dataSource = app.usersModel.usersDataSource;
             var data = dataSource.view();
 
@@ -50,9 +26,12 @@ app.usersModel = (function (global) {
             });
 
             return checkedIds;
-        }
-    });
-
-    return usersViewModel;
-
-})(window);
+        };
+    
+        return {
+            usersDataSource : app.usersDataSource,
+            onUserSelected : onUserSelected,
+            getSelectedUsersFromDataSource : getSelectedUsersFromDataSource
+        };
+    }());
+}(window));
