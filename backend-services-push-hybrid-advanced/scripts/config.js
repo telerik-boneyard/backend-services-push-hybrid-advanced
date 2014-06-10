38c79a4cc1bb4a933e9a2ab80d1dc3fd89fe1ce9
Anton Dobrev <anton.dobrev@telerik.com>
2014-06-10 08:29:59 -0500

    Placed the placeholders.

--- backend-services-push-hybrid-advanced/scripts/config.js
+++ backend-services-push-hybrid-advanced/scripts/config.js
@@ -6,7 +6,7 @@
     app.config = {
 
         everlive: {
-            apiKey: 'lGZ6g8a95ovHKepC', // Put your Backend Services API key here
+            apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
             scheme: 'http'
         },
 
@@ -19,7 +19,7 @@
         }
     };
 
-    app.androidProjectNumber = '406987471724';
+    app.androidProjectNumber = "ANDROID_PROJECT_NUMBER";
 
     app.constants = {
         NO_API_KEY_MESSAGE: '<h3>Backend Services <strong>API Key</strong> is not set.</h3><p><span>API Key</span> links the sample mobile app to a project in Telerik Backend Services.</p><p>To set the <span>API Key</span> open the <span>/scripts/app/config.js</span> file and replace <strong>$EVERLIVE_API_KEY$</strong> with the <span>API Key</span> of your Backend Services project.</p>',
--- backend-services-push-hybrid-advanced/scripts/push-factory.js
+++ backend-services-push-hybrid-advanced/scripts/push-factory.js
@@ -43,7 +43,6 @@
                     "sound": "default"
                 },
                 "customData": customData
-                
             };
 
             var notificationObject = {
