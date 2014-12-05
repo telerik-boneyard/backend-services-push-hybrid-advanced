7dfb698b877443db257f49dd516ee5ba5f8927a4
Anton Dobrev <anton.dobrev@telerik.com>
2014-12-05 16:04:07 +0000

    Configuration file.

--- backend-services-push-hybrid-advanced/scripts/config.js
+++ backend-services-push-hybrid-advanced/scripts/config.js
@@ -1,15 +1,11 @@
 (function (global) {
     'use strict';
-
     var app = global.app = global.app || {};
-
     app.config = {
-
         everlive: {
             apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
             scheme: 'http'
         },
-
         views: {
             init: '#initView',
             noApiKey: 'views/noApiKeyView.html',
@@ -18,12 +14,9 @@
             main: 'views/mainView.html'
         }
     };
-
     app.androidProjectNumber = "ANDROID_PROJECT_NUMBER";
-
     app.constants = {
         NO_API_KEY_MESSAGE: '<h3>Backend Services <strong>API Key</strong> is not set.</h3><p><span>API Key</span> links the sample mobile app to a project in Telerik Backend Services.</p><p>To set the <span>API Key</span> open the <span>/scripts/config.js</span> file and replace <strong>$EVERLIVE_API_KEY$</strong> with the <span>API Key</span> of your Backend Services project.</p>',
         EMULATOR_MODE: false
     };
-
 }(window));
