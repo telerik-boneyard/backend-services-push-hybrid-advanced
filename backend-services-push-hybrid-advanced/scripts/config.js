49798761e4d1b12b0a6a36f3bf90f39e6922ff75
Anton <ant.dobrev@gmail.com>
2015-12-09 13:19:56 +0200

    Update error message for Telerik Platform app

--- backend-services-push-hybrid-advanced/scripts/config.js
+++ backend-services-push-hybrid-advanced/scripts/config.js
@@ -17,7 +17,7 @@
     app.androidProjectNumber = "ANDROID_PROJECT_NUMBER";
     app.constants = {
         NO_APP_ID_MESSAGE: '<h3>Telerik Platform <strong>App ID</strong> is not set.</h3><p><span>App ID</span> ' +
-        'links the sample mobile app to a project in Telerik Backend Services.</p><p>To set the <span>App ID</span> ' +
+        'links the sample mobile app to a Telerik Platform app.</p><p>To set the <span>App ID</span> ' +
         'open the <span>/scripts/config.js</span> file and replace <strong>$TELERIK_APP_ID$</strong> with the ' +
         '<span>App ID</span> of your Telerik app.</p>',
         EMULATOR_MODE: false
